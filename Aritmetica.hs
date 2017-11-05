48c2ff298816cf76d663b29e8637e731c577c0bd
JuanLucasLaur <33398520+JuanLucasLaur@users.noreply.github.com>
2017-11-05 14:12:27 -0300

    Agregado ejercicio 2 (criba)

--- Aritmetica.hs
+++ Aritmetica.hs
@@ -9,8 +9,12 @@ mcdExt :: Integer -> Integer -> (Integer, (Integer, Integer))
 mcdExt _ _ = (0, (0, 0))
 
 --(2)
+-- Dado un entero, devuelve un conjunto con todos los números primos
+-- menores a él.
 criba :: Integer -> Set Integer
-criba _ = []
+criba 2 = []
+criba n | esPrimo (n - 1) = (n - 1) : criba (n - 1)
+        | otherwise = criba (n - 1)
 
 --(3)
 coprimoCon:: Integer -> Integer
@@ -28,3 +32,23 @@ modExp :: Integer -> Integer -> Integer -> Integer
 modExp b 0 m = 1
 modExp b e m = t * modExp ((b * b) `mod` m) (shiftR e 1) m `mod` m
   where t = if testBit e 0 then b `mod` m else 1
+  
+{- Función sumaDivisores - Devuelve la suma de todos los divisores de un número dado -}
+sumaDivisores :: Integer -> Integer
+sumaDivisores n = sumaDivisoresHasta (abs(n)) (abs(n))
+
+{- Función sumaDivisoresHasta
+ - Realiza una suma entre los divisores de un número que sean menores
+ - a otro número dado (que representa el número máximo desde el que se
+ - comienzan a buscar divisores y se va reduciendo con cada ejecución)
+ -}
+sumaDivisoresHasta :: Integer -> Integer -> Integer
+sumaDivisoresHasta dividendo divisorLimite | divisorLimite == 0 = 0
+                                           | divisorLimite == 1 = 1                                           
+                                           | dividendo `mod` divisorLimite == 0 = (sumaDivisoresHasta dividendo (divisorLimite - 1)) + divisorLimite
+                                           | otherwise = sumaDivisoresHasta dividendo (divisorLimite - 1)
+                                           
+{- Función esPrimo
+ - Recibe un número entero y devuelve True si es primo -}
+esPrimo :: Integer -> Bool
+esPrimo n = sumaDivisores (abs(n)) == abs(n) + 1
