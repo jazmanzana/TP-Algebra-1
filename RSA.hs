7fa25dd824e1568b5ccada7f11c99e7f1e2da265
jazmanzana <19Kiel12>
2017-11-13 19:20:50 -0300

    mejora de Juan a claves y comentarios ordenaditos

--- RSA.hs
+++ RSA.hs
@@ -7,26 +7,28 @@ import Aritmetica
 claves :: Integer -> Integer -> (Integer, Integer, Integer)
 claves p q = (e, d, n)
   where n = p*q
-        (e, d) = moduloPhi ((p-1)*(q-1)) ((p-1)*(q-1)) -- llamo con phi dos veces, una es mi e que le voy restando 1
-
--- d es el inverso de e modulo phi = calculo d con inversoMultiplicativo
--- e es un numero entre 0 y phi que cumpla con la ecuacion de congruencia dada
-moduloPhi :: Integer -> Integer -> (Integer, Integer)
-moduloPhi 0 phi = (0, inversoMultiplicativo 0 phi) -- revisar caso base: tiene que ser con e 0 pero que devuelvo en d?
-moduloPhi e phi | esCongruenteAUno (e* (inversoMultiplicativo e phi)) phi = (e, (inversoMultiplicativo e phi)) --if True, devuelve e y d
-                | otherwise = moduloPhi (e-1) phi --if False, probamos con e mas pequenio
-
--- llamo a esCongruenteAUno con e*d y phi (que es el modulo)
-esCongruenteAUno :: Integer -> Integer -> Bool
-esCongruenteAUno a phi | mod a phi == mod 1 phi = True
-                       | otherwise = False
+        (e, d) = calcular_e_d ((p-1)*(q-1)) ((p-1)*(q-1)) -- llamo con phi dos veces, una es mi e que le voy restando 1
 
 
+{- Recibe dos números, `f` y φ y devuelve los valores `e`, `d` utilizados
+ - por RSA
+ -}
+calcular_e_d :: Integer -> Integer -> (Integer, Integer)
+calcular_e_d 0 _ = (0, 0)
+calcular_e_d f phi | abs (inversoMultiplicativo f phi) /= 1 = (f, inversoMultiplicativo f phi)
+                   | otherwise = calcular_e_d (f - 1) phi
+
+
+
+--(6)
+-- Dada la clave pública y una secuencia de caracteres, la codifica y devuelve la secuencia obtenida.
 codificador :: ClPub -> Mensaje -> Cifrado
 codificador kPublic mensaje | fst(mcdExt snd(kPublic) (aEnteros mensaje)) == 1 = convertirLista (aEnteros mensaje) kPublic 
 							| otherwise = -(aEnteros mensaje)
 
-							
+--(7)
+-- Dados una clave privada y un mensaje cifrado devuelve un mensaje
+-- decodificado
 decodificador :: ClPri -> Cifrado -> Mensaje
 decodificador kPriv mCifrado | mCifrado >= 0 = revertirLista mCifrado ClPri
 							 | otherwise = aChars (- mCifrado) 
